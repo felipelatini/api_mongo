070a12db5910af7b8c97585c6ac02ad4c6a2a59d
Felipe Latini Gaudencio <felipelgaudencio@gmail.com>
2021-04-19 20:31:27 -0300

    commit inicial

---
+++ Api/Controllers/InfectadoController.cs
@@ -0,0 +1,62 @@
+﻿using Api.Data.Collections;
+using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{    
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB              = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+
+            return StatusCode(201, "Infectado alterado com sucesso");
+        }
+
+        [HttpDelete("{dataNascimento}")]
+        public ActionResult DeletarInfectado(DateTime dataNascimento)
+        {
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dataNascimento));
+
+            return StatusCode(201, "Infectado deletado com sucesso");
+        }
+    }
+}
